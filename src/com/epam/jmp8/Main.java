b878e6b1a70c8343ff24a82032aecf64623d6e2c
Vitali Ihnatsenka <vitt.ignat@gmail.com>
2016-09-03 18:43:20 +0300

    task

--- src/com/epam/jmp8/Main.java
+++ src/com/epam/jmp8/Main.java
@@ -1,8 +1,40 @@
 package com.epam.jmp8;
 
 public class Main {
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
 
     public static void main(String[] args) {
-	// write your code here
+	    new Thread(() -> {
+            synchronized (lock1){
+                System.out.println("Thread 1: Holding lock 1...");
+                try {
+                    Thread.sleep(10);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Thread 1: Waiting for lock 2...");
+                synchronized (lock2) {
+                    System.out.println("Thread 1: Holding lock 1 & 2...");
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (lock2){
+                System.out.println("Thread 2: Holding lock 2...");
+                try {
+                    Thread.sleep(10);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Thread 2: Waiting for lock 1...");
+                synchronized (lock1) {
+                    System.out.println("Thread 2: Holding lock 1 & 2...");
+                }
+            }
+        }).start();
     }
 }
